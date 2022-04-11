13a532e1e069fa0f8538976e3078e8725723db2e
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-11 08:34:43 +0100

    refactor(bufferline): use is_tabline method

--- lua/bufferline.lua
+++ lua/bufferline.lua
@@ -120,8 +120,8 @@ end
 
 --- If the item count has changed and the next tabline status is different then update it
 function M.toggle_bufferline()
-  local nitems = config.options.mode == "tabs" and utils.get_tab_count() or utils.get_buf_count()
-  local status = (config.options.always_show_bufferline or nitems > 1) and 2 or 0
+  local item_count = config:is_tabline() and utils.get_tab_count() or utils.get_buf_count()
+  local status = (config.options.always_show_bufferline or item_count > 1) and 2 or 0
   if vim.o.showtabline ~= status then
     vim.o.showtabline = status
   end
